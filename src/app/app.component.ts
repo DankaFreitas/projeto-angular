b150ff8b20efecedf70cd6b002705b719f48f139
Oberdã <carlos-oberda@hotmail.com>
2022-11-28 11:25:45 -0300

    Compartilhamento de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName  = 'Britney';
+  userData = {
+    email: 'britney@email.com',
+    role: 'Admin',
+  }
   title = 'projeto';
 }
